de013361625105678ff1bfcf1a1aad8aba09d9e6
Rahul Naik <73591592+Rahul-naik92@users.noreply.github.com>
2024-03-05 07:24:51 +0000

    usage of const, let, var in js

---
+++ .devcontainer/js_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId=12345;
+let accountEmail="rahul@gmail.com";
+var accountPassword="P@aasswordd"
+accountCity="Bangalore"
+
+//accountId=23566;//not allowed
+
+
+accountEmail="hc@hc.com";
+accountPassword='P@sw0rddd0';
+accountCity="Jaipur"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
