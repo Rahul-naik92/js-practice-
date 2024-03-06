91ecb86f1b984047b23c5ca3c0608f3d0321e9f7
Rahul Naik <73591592+Rahul-naik92@users.noreply.github.com>
2024-03-06 09:57:30 +0000

    datatypes

---
+++ .devcontainer/js_basics/02_01_newstandards.js
@@ -0,0 +1,19 @@
+"use strict";//treat all the code as a newer version
+
+//alert(2+2) // we are not using browser here, we are running the code in node js
+
+console.log(3+3);
+let num =31
+let name="Rahul"
+let isLoggedIn=false;
+
+//int
+//bigInt
+//string
+//bool
+//null --object and standalone value
+//undefined
+
+//object:
+
+console.log(typeof undefined);
